7e0ae0742b76396dfdcb9ffa522e2f854fdf8bbc
MD DILDAR MANDAL <dildarmandal335@gmail.com>
2023-11-13 04:29:15 +0530

    feat!: add file
    # Description
    Type: html, css, js

---
+++ script.js
@@ -0,0 +1,35 @@
+
+//click on new note
+let neww = document.querySelector('.new-btn');
+let newNotes = document.querySelector('.new-notes');
+neww.onclick = () =>{
+    saveNotes.classList.remove('active');
+    about_con.classList.remove('active');
+
+    newNotes.classList.toggle('active');
+}
+
+//click on saved
+
+let save = document.querySelector('.saved-btn');
+let saveNotes = document.querySelector('.saved');
+save.onclick = () =>{
+    newNotes.classList.remove('active');
+    about_con.classList.remove('active');
+
+    saveNotes.classList.toggle('active');
+}
+
+//click on about
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let about = document.querySelector('.about');
+let about_con = document.querySelector('.about-con');
+about.onclick = async () =>{
+    about_con.classList.toggle('active');
+    await sleep(1500);
+    about_con.classList.remove('active');
+
+}
